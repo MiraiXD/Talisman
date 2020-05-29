4a1677760b9e82302b42b4b05d3e4ad3d53f58ea
kkrawczykHome <kkrawczyk101@gmail.com>
2020-05-29 20:04:52 +0200

    stuff

---
+++ Assets/CharacterCard.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Characters { Warrior, Mage, Ogre }
+public class CharacterCard : MonoBehaviour
+{
+    public Characters characters;
+}
